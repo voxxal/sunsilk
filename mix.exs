cde3ddbaecc113c120a085ea6d99234b58e08cb3
voxal <aidenhohoho@gmail.com>
2023-03-08 13:33:44 -0800

    add veloren assets

--- mix.exs
+++ mix.exs
@@ -46,7 +46,7 @@ defmodule Sunsilk.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
       {:nimble_parsec, "~> 1.0"}
     ]
   end
